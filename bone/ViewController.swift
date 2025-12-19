d719ace22b2df288151168e83c2c89600097e4bf
Park Gwonyong <rnjsdydqkr@naver.com>
2025-12-19 23:10:21 +0900

    Initial Commit

---
+++ bone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bone
+//
+//  Created by 박권용 on 12/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ boneTests/boneTests.swift
@@ -0,0 +1,17 @@
+//
+//  boneTests.swift
+//  boneTests
+//
+//  Created by 박권용 on 12/19/25.
+//
+
+import Testing
+@testable import bone
+
+struct boneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
