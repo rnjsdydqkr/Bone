62b588c6e187249858445d41e6e4fa8f8f8cab28
Park KwonYong <rnjsdydqkr@naver.com>
2026-09-17 18:01:31 +0900

    Splash 화면 MVVM Combine 적용중

--- Bone/Presentation/Splash/SplashViewController.swift
+++ Bone/Presentation/Splash/SplashViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import KYKit
+import Combine
 
 final class SplashViewController: KYViewController {
 
@@ -18,11 +19,17 @@ final class SplashViewController: KYViewController {
 	// MARK: - Properties
 
 	private let viewModel = SplashViewModel()
+  
+  private let viewDidAppearSubject = PassthroughSubject<Void, Never>()
+  
+  private let moveScreenButtonTappedSubject = PassthroughSubject<Void, Never>()
+  private let moveScreenTwoButtonTappedSubject = PassthroughSubject<Void, Never>()
 
 	// MARK: - Life Cycle
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+    bindViewModel()
 	}
 
 	override func viewWillAppear(_ animated: Bool) {
@@ -38,12 +45,21 @@ final class SplashViewController: KYViewController {
 
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
-		Task {
-			await viewModel.delaySplash(.main)
-		}
+    viewDidAppearSubject.send()
 	}
 
 	// MARK: - Setup (초기 세팅)
+  
+  private func bindViewModel() {
+    let input = SplashViewModel.Input(
+      viewDidAppear: viewDidAppearSubject.eraseToAnyPublisher(),
+      moveScreenButtonTapped: moveScreenButtonTappedSubject.eraseToAnyPublisher(),
+      moveScreenTwoButtonTapped: moveScreenTwoButtonTappedSubject.eraseToAnyPublisher()
+    )
+    
+    let output = viewModel.transform(input: input)
+
+  }
 
 	// MARK: - Binding (ViewModel 바인딩 / 구독)
 
@@ -59,8 +75,8 @@ final class SplashViewController: KYViewController {
 
 	@IBAction func didTapButton(_ sender: UIButton) {
 		switch sender {
-		case moveScreenButton: break
-		case moveScreenTwoButton: break
+		case moveScreenButton: moveScreenButtonTappedSubject.send()
+    case moveScreenTwoButton: moveScreenTwoButtonTappedSubject.send()
 		default: break
 		}
 	}
--- Bone/Presentation/Splash/SplashViewModel.swift
+++ Bone/Presentation/Splash/SplashViewModel.swift
@@ -5,23 +5,55 @@
 //  Created by 박권용 on 12/23/25.
 //
 
-import Combine
 import Foundation
 import KYKit
+import Combine
 
-@MainActor final class SplashViewModel {
-
+@MainActor final class SplashViewModel: ViewModel {
 	// MARK: - Input / Output
+  
+  struct Input {
+    let viewDidAppear: AnyPublisher<Void, Never>
+    let moveScreenButtonTapped: AnyPublisher<Void, Never>
+    let moveScreenTwoButtonTapped: AnyPublisher<Void, Never>
+    
+  }
+  
+  struct Output {
+    
+  }
 
 	// MARK: - Properties
+  
+  private var cancellable = Set<AnyCancellable>()
 
 	// MARK: - Initializer
 
 	// MARK: - Binding (Input → Output)
+  func transform(input: Input) -> Output {
+    input.viewDidAppear
+      .sink { [weak self] _ in
+        Task {
+          await self?.delaySplash(.main)
+        }
+      }.store(in: &cancellable)
+    
+    input.moveScreenButtonTapped
+      .sink { [weak self] _ in
+        print("moveScreenButtonTapped")
+      }.store(in: &cancellable)
+    
+    input.moveScreenTwoButtonTapped
+      .sink { [weak self] _ in
+        print("moveScreenTwoButtonTapped")
+      }.store(in: &cancellable)
+    
+    return Output()
+  }
 
 	// MARK: - Function (기능 로직 / 화면 이동)
 
-	func delaySplash(_ screenType: ScreenType) async {
+	private func delaySplash(_ screenType: ScreenType) async {
 		try? await Task.sleep(for: .seconds(0.5))
 		await MainActor.run {
 			self.changeScreen(screenType)
